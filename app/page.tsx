8e79d9d2005085868f4441611f193df257223926
w4rdr43w4n <zwerdos123@gmail.com>
2024-09-18 20:54:48 +0300

    No Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import GenerateField from "@/components/Preview/GenerateField";
 import Preview from "@/components/Preview/Preview";
 import { useState } from "react";
@@ -6,15 +7,17 @@ import { useState } from "react";
 export default function Home() {
   const [code, setCode] = useState("");
   return (
-    <main className="flex flex-col items-center justify-center p-8 gap-10">
-      <h1
-        className="font-bold text-xl text-sky-500"
-        style={{ fontSize: "45px" }}
-      >
-        Landing Page Generator
-      </h1>
-      <GenerateField setCode={setCode} />
-      <Preview code={code} />
-    </main>
+    <>
+      <main className="flex flex-col items-center justify-center p-8 gap-10">
+        <h1
+          className="font-bold text-xl text-sky-500"
+          style={{ fontSize: "45px" }}
+        >
+          Landing Page Generator
+        </h1>
+        <GenerateField setCode={setCode} />
+        <Preview code={code} />
+      </main>
+    </>
   );
 }
